ef0fd8d823c15259f86757d03e9323d4e592a00e
Leo Denault <leo.denault@gmail.com>
2014-12-02 18:47:42 -0500

    Converted remaining JS to JQuery

--- static/handlers.js
+++ static/handlers.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 	
 	invVarParent = $("#invariants");
 	invVarParentBlock = $("#invVarParentBlock");
+	invVarParentBlock.hide();
 	
 	var program = $("#program");
 	program.on("input", countWhileLoops);
@@ -22,8 +23,8 @@ $(document).ready(function () {
 });
 
 function countWhileLoops() {
-	var program = document.getElementById("program");
-	var text = program.value;
+	var program = $("#program");
+	var text = program.val();
 	
 	var whiles = (text.match(/(\s+|^)while((\s*\()|\s+((true)|(false)))/g) || []).length;
 	
@@ -36,44 +37,49 @@ function generateInvVarInputs(count) {
 	invVarParent.empty();
 	
 	if (count > 0) {
-		invVarParentBlock.addClass("col-sm-6");
-		invVarParentBlock.removeClass("hidden");
+		invVarParentBlock.show(300);
 	} else {
-		invVarParentBlock.removeClass("col-sm-6");
-		invVarParentBlock.addClass("hidden");
+		invVarParentBlock.hide(300);
 	}
 	
 	for (var i = 0; i < count; i++) {
-		var invInput = document.createElement("input");
-		invInput.name = "invariant" + i;
-		invInput.type = "text";
-		invInput.placeholder = "Invariant " + (i + 1);
-		invInput.required = true;
-		invInput.className = "form-control";
+		var invInput =
+			$("<input/>")
+				.addClass("form-control")
+				.attr({
+					name:			"invariant" + i,
+					type:			"text",
+					placeholder:	"Invariant " + (i + 1),
+					required:		true
+			});
+		var varInput =
+			invInput
+				.clone()
+				.attr({
+					name:			"variant" + i,
+					placeholder:	"Variant " + (i + 1)
+				});
 		
+		var invDiv =
+			$("<div/>")
+				.addClass("col-sm-6")
+				.append(invInput);
+		var varDiv =
+			invDiv
+				.clone()
+				.empty()
+				.append(varInput);
 		
-		var varInput = document.createElement("input");
-		varInput.name = "variant" + i;
-		varInput.type = "text";
-		varInput.placeholder = "Variant " + (i + 1);
-		varInput.required = true;
-		varInput.className = "form-control";
-		
-		var invDiv = document.createElement("div")
-		var varDiv = document.createElement("div")
-		invDiv.className = "col-sm-6";
-		varDiv.className = "col-sm-6";
-		invDiv.appendChild(invInput);
-		varDiv.appendChild(varInput);
-		
-		var div = document.createElement("div");
-		div.className = "row top-bottom-padded";
-		div.appendChild(invDiv);
-		div.appendChild(varDiv);
-		var spacer = document.createElement("div")
-		spacer.className = "col-md-12";
-		spacer.appendChild(div);
-		invVarParent.append(spacer);
+		invVarParent.append(
+			$("<div/>")
+				.addClass("col-md-12")
+				.append(
+					$("<div/>")
+						.addClass("row")
+						.addClass("top-bottom-padded")
+						.append(invDiv)
+						.append(varDiv)
+				));
 	}
 }
 
